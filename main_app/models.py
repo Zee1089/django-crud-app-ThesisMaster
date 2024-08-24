f08d3dea9e192562d113b0d5bd48154e4a5e629d
Zee <zeeboo696@gmail.com>
2024-08-24 19:11:25 -0400

    Add Django Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class ResearchPaper(models.Model):
+    title = models.CharField(max_length=250)
+    authors = models.TextField()  # Changed to TextField for potentially longer author lists
+    journal = models.CharField(max_length=250)  # Added field for journal name
+    publication_date = models.DateField()  # Changed to DateField to store dates
+    major_findings = models.TextField()  # Changed to TextField for textual data
+
+    def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -8,9 +8,10 @@ from django.http import HttpResponse
 # views.py
 
 class ResearchPaper:
-    def __init__(self, title, authors, publication_date, major_findings):
+    def __init__(self, title, authors, journal, publication_date, major_findings):
         self.title = title
         self.authors = authors
+        self.journal = journal
         self.publication_date = publication_date
         self.major_findings = major_findings
 
@@ -19,29 +20,32 @@ research_papers = [
     ResearchPaper(
         "Neural Mechanisms of Anorexia",
         ["Dr. Jane Smith", "Dr. John Doe"],
+        "Journal of Neuroscience",
         "2023-04-15",
         "Identified altered activity in the hypothalamus linked to appetite suppression."
     ),
     ResearchPaper(
         "Cognitive Behavioral Therapy for PTSD",
         ["Dr. Emily Johnson", "Dr. Michael Lee"],
+        "Journal of Clinical Psychology",
         "2022-10-20",
         "Showed significant improvement in PTSD symptoms after 12 weeks of therapy."
     ),
     ResearchPaper(
         "The Role of Gut Microbiota in Depression",
         ["Dr. Susan Martinez", "Dr. Robert Brown"],
+        "Journal of Psychiatry & Neuroscience",
         "2021-07-05",
         "Demonstrated a correlation between gut microbiota composition and depressive behavior in mice."
     ),
     ResearchPaper(
         "Genetic Variants Associated with Schizophrenia",
         ["Dr. Linda Green", "Dr. Richard White"],
+        "Journal of Genetic Research",
         "2020-11-30",
         "Discovered several genetic variants significantly associated with an increased risk of schizophrenia."
     )
 ]
-
 
 
 # Define the home view function
