9f605bef51dadbb437fe30d132f8b2eadb2eac95
Zee <zeeboo696@gmail.com>
2024-08-27 23:57:14 -0400

    static route

--- ThesisMaster/settings.py
+++ ThesisMaster/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 LOGIN_URL = 'home'
 # Add this variable to specify where successful logins should redirect to
